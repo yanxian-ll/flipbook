e5f6dcd9d341e83e3a2fca49f61ad02e58cb6bf2
yanxian <yanxiancsu@csu.edu.cn>
2026-09-10 23:54:20 +0800

    feat(desktop): expose close window control

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,5 +1,6 @@
 const {contextBridge,ipcRenderer}=require('electron');
 
 contextBridge.exposeInMainWorld('desktopWindow',{
+  close:()=>ipcRenderer.invoke('desktop-window:close'),
   setExpanded:(expanded)=>ipcRenderer.invoke('desktop-window:set-expanded',Boolean(expanded)),
 });
