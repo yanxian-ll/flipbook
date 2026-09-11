3959a2a705855e855b4ec8461b8c24a1da93688f
yanxian <yanxiancsu@csu.edu.cn>
2026-09-11 10:33:22 +0800

    feat(desktop): expose unified workspace toggle

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -2,5 +2,12 @@ const {contextBridge,ipcRenderer}=require('electron');
 
 contextBridge.exposeInMainWorld('desktopWindow',{
   close:()=>ipcRenderer.send('desktop-window:close'),
+  getExpanded:()=>ipcRenderer.invoke('desktop-window:get-expanded'),
   setExpanded:(expanded)=>ipcRenderer.invoke('desktop-window:set-expanded',Boolean(expanded)),
+  toggleExpanded:()=>ipcRenderer.invoke('desktop-window:toggle-expanded'),
+  onExpandedChanged:(listener)=>{
+    const wrapped=(_event,expanded)=>listener(Boolean(expanded));
+    ipcRenderer.on('desktop-window:expanded-changed',wrapped);
+    return()=>ipcRenderer.removeListener('desktop-window:expanded-changed',wrapped);
+  },
 });
